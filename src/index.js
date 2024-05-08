0d4ea968741d4672e079d21a84bc6f51c0478cb8
Brian Smith <bsmith@axim.org>
2024-05-08 19:30:41 -0400

    feat: provide `FooterSlot`
    This allows consuming MFEs to utilize a standardized Frontend
    Plugin Framework `PluginSlot` for replacing the footer.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Footer, { EVENT_NAMES } from './components/Footer';
 import messages from './i18n/index';
 import StudioFooter from './components/studio-footer';
+import FooterSlot from './components/footer-slot';
 
 export default Footer;
-export { messages, EVENT_NAMES, StudioFooter };
+export {
+  messages, EVENT_NAMES, StudioFooter, FooterSlot,
+};
