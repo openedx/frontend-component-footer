12527ee3527f3ee718212f723d8fe262086b7f8c
Diana Olarte <diana.olarte@edunext.co>
2025-01-13 12:51:57 +1100

    refactor: change lang selector logic

--- src/components/Footer.test.jsx
+++ src/components/Footer.test.jsx
@@ -1,11 +1,20 @@
 /* eslint-disable react/prop-types */
 import React, { useMemo } from 'react';
 import renderer from 'react-test-renderer';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
+import { initializeMockApp } from '@edx/frontend-platform/testing';
 import { IntlProvider } from '@edx/frontend-platform/i18n';
+import { getCookies } from '@edx/frontend-platform/i18n/lib';
 import { AppContext } from '@edx/frontend-platform/react';
+import '@testing-library/jest-dom';
 
 import Footer from './Footer';
+import { patchPreferences, postSetLang } from './LanguageSelector/data';
+
+jest.mock('./LanguageSelector/data', () => ({
+  patchPreferences: jest.fn(),
+  postSetLang: jest.fn(),
+}));
 
 const FooterWithContext = ({ locale = 'es' }) => {
   const contextValue = useMemo(() => ({
@@ -27,33 +36,36 @@ const FooterWithContext = ({ locale = 'es' }) => {
   );
 };
 
-const FooterWithLanguageSelector = ({ languageSelected = () => {} }) => {
+const { LANGUAGE_PREFERENCE_COOKIE_NAME } = process.env;
+const FooterWithLanguageSelector = ({ authenticatedUser = null }) => {
   const contextValue = useMemo(() => ({
-    authenticatedUser: null,
+    authenticatedUser,
     config: {
+      ENABLE_FOOTER_LANG_SELECTOR: true,
+      LANGUAGE_PREFERENCE_COOKIE_NAME,
       LOGO_TRADEMARK_URL: process.env.LOGO_TRADEMARK_URL,
       LMS_BASE_URL: process.env.LMS_BASE_URL,
+      SITE_SUPPORTED_LANGUAGES: ['es', 'en'],
     },
-  }), []);
+  }), [authenticatedUser]);
 
   return (
     <IntlProvider locale="en">
       <AppContext.Provider
         value={contextValue}
       >
-        <Footer
-          onLanguageSelected={languageSelected}
-          supportedLanguages={[
-            { label: 'English', value: 'en' },
-            { label: 'Español', value: 'es' },
-          ]}
-        />
+        <Footer />
       </AppContext.Provider>
     </IntlProvider>
   );
 };
 
 describe('<Footer />', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    initializeMockApp();
+  });
+
   describe('renders correctly', () => {
     it('renders without a language selector', () => {
       const tree = renderer
@@ -76,21 +88,32 @@ describe('<Footer />', () => {
   });
 
   describe('handles language switching', () => {
-    it('calls onLanguageSelected prop when a language is changed', () => {
-      const mockHandleLanguageSelected = jest.fn();
-      render(<FooterWithLanguageSelector languageSelected={mockHandleLanguageSelected} />);
+    it('calls publish with LOCALE_CHANGED when the language changed', () => {
+      const setSpy = jest.spyOn(getCookies(), 'set');
+      const component = render(<FooterWithLanguageSelector />);
 
-      fireEvent.submit(screen.getByTestId('site-footer-submit-btn'), {
-        target: {
-          elements: {
-            'site-footer-language-select': {
-              value: 'es',
-            },
-          },
-        },
+      expect(component.queryByRole('button')).toBeInTheDocument();
+
+      const langDropdown = component.queryByRole('button');
+      fireEvent.click(langDropdown);
+      fireEvent.click(component.queryByText('Español'));
+
+      expect(setSpy).toHaveBeenCalledWith(LANGUAGE_PREFERENCE_COOKIE_NAME, 'es');
+    });
+    it('update the lang preference for an autheticathed user', async () => {
+      const userData = { username: 'test-user' };
+      const component = render(<FooterWithLanguageSelector authenticatedUser={userData} />);
+
+      expect(component.queryByRole('button')).toBeInTheDocument();
+
+      const langDropdown = component.queryByRole('button');
+      fireEvent.click(langDropdown);
+      fireEvent.click(component.queryByText('Español'));
+
+      await waitFor(() => {
+        expect(patchPreferences).toHaveBeenCalledWith(userData.username, { prefLang: 'es' });
+        expect(postSetLang).toHaveBeenCalledWith('es');
       });
-
-      expect(mockHandleLanguageSelected).toHaveBeenCalledWith('es');
     });
   });
 });
